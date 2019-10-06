65c2b1aa85a07d9ed97756641c485f9042fc4d51
Masashi <mapi0213mgt@gmail.com>
2019-10-06 20:34:27 +0900

    サインアップ画面,ログイン画面作成

---
+++ web/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <b-navbar toggleable="lg" type="dark" variant="success">
+    <b-navbar-brand to="#">
+    Portfolio
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item to="/signup">Signup</b-nav-item>   
+        <b-nav-item to="/login">Login</b-nav-item>   
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+logo img{
+
+}
+</style>
--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,15 +1,54 @@
 <template>
-  <div>
-    <h1>ログイン</h1>
-    <lable for="id">Id:</lable>
-    <input id="id" placeholder="id" type="text" v-model="id" />
-    <br />
-    <label for="password">Password:</label>
-    <input id="password" placeholder="password" type="text" v-model="password" />
-    <button @click="login">ログイン</button>
-    <hr />
-    <router-link to="/">もどる</router-link>
+<div>
+  <b-card-group deck class="my-5">
+    <b-card 
+    bg-variant="dark" 
+    header="Welcome to My Portfolio!"
+    align="center"
+    text-variant="white"
+    header-border-variant="white"
+    style="max-width: 30rem;"
+    class="mx-auto"
+    >
+    <b-card-text>
+        <input class="input form-control" type="text" placeholder="test@test.com" v-model="id" />
+        <div class="my-2">
+        <input class="form-control" type="password" placeholder="password" v-model="password" />
+        </div>
+    </b-card-text>
+            <button class="btn btn-success btn-block">Login</button>
+    </b-card>
+  </b-card-group>
+  <div class="text-center mt-n4">
+    <router-link to="signup">
+      Create New Account
+      </router-link>
   </div>
+</div>
+
+
+<!-- 
+  <form action>
+    <h3 class="card-header bg-transparent font-weight-bold">Login</h3>
+    <div class="card-body mx-auto col-sm-8 col-xs-12">
+      <div class="form-label-group my-2">
+        <label class="label">ID</label>
+        <input class="input form-control" type="text" placeholder="test@test.com" v-model="id" />
+      </div>
+      <div class="form-label-group my-2">
+        <label class="label">PASSWORD</label>
+        <input class="form-control " type="password" placeholder="password" v-model="password" />
+        <div class="text-right">
+        </div>
+      </div>
+      <div class="card-footer bg-transparent text-center">
+        <button class="btn btn-outline-success btn-lg">Login</button>
+        <br/>
+        <button class="btn btn-success btn-lg my-2">アカウント登録</button>
+      </div>
+      
+    </div>
+  </form> -->
 </template>
 
 <script>
@@ -17,19 +56,18 @@ export default {
   data() {
     return {
       id: "",
-      password: "",
+      password: ""
     };
   },
-    methods: {
+  methods: {
     login() {
-      this.$store.dispatch('login', {
-        id : this.id,
-        password: this.password,
-      })
-    },
+      this.$store.dispatch("login", {
+        id: this.id,
+        password: this.password
+      });
+    }
   }
 };
-
 </script>
 
 <style>
